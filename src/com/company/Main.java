933ea5e9368bdc9457352cd3f450d69ece705fa0
Malkollm <fatkullin@bk.ru>
2016-02-02 23:20:19 +0300

    работа с массивом

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("What the fuck?");
+
+        int[] arr = new int[100];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = i*8;
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("\n");
+        System.out.print(arr[23]);
     }
 }
